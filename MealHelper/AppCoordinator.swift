624c186994270d896ed7c905421504cb0af97928
DenisYuminov <97443918+DenisYuminov@users.noreply.github.com>
2023-05-21 15:09:35 +0300

    bug fix with switch coordinators

--- MealHelper/AppCoordinator.swift
+++ MealHelper/AppCoordinator.swift
@@ -10,16 +10,32 @@ import UIKit
 final class AppCoordinator {
     // Dependencies
     private let authCoordinator: IAuthCoordinator
+    private let mainTabBarCoordinator: IMainTabBarCoordinator
+    
+    // Properties
+    var navigationContonroller: UINavigationController
     
     // MARK: Init
     
-    init(appPresentationAssembly: IAppPresentationAssembly) {
+    init(
+        appPresentationAssembly: IAppPresentationAssembly,
+        navigationContonroller: UINavigationController
+    ) {
         self.authCoordinator = appPresentationAssembly.authCoordinator
+        self.navigationContonroller = navigationContonroller
+        self.mainTabBarCoordinator = appPresentationAssembly.mainTabBarCoordinator
     }
     
     // MARK: Internal
     
-    func createRootViewController() -> UIViewController {
-        authCoordinator.createFlow()
+    func createRootViewController() {
+        let isAuth = false
+        if isAuth {
+            let viewController = mainTabBarCoordinator.createFlow(isAuth: isAuth)
+            navigationContonroller.pushViewController(viewController, animated: true)
+        } else {
+            let viewController = authCoordinator.createFlow()
+            navigationContonroller.pushViewController(viewController, animated: true)
+        }
     }
 }
